ffce747fced156d81216fac8169f84aacf5b10af
Valeriy Gogin <qbarez@yandex.ru>
2023-02-04 21:34:04 +0300

    add ReactContext

---
+++ src/components/landing/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+import {TranslationContext} from '../../contexts/translation/TranslationContext';
+
+function Header() {
+
+  const translation = React.useContext(TranslationContext);
+
+  return (
+    <h1 className="header-title">{translation.title}</h1>
+  );
+}
+
+export default Header;
